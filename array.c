a56d884892dc907487ed1e5aebbd8b77ea097bfd
RumazQureshi <rumazqureshi@gmail.com>
2025-03-25 04:03:23 +0500

    initial commit

---
+++ array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void odds(int array ,int n);
+int main(){
+odds();
+    return 0;
+}
+void odds(int array ,int n){
+for (int i = 1; i < n; i++)
+{
+   if (n%2!=0){
+    int sum=array+i;
+
+   }
+   printf("%d\n",array);
+}
+
+
+}
