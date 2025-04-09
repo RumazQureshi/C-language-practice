2f200b5f622a0feed469cbc5c7f67a13a997303a
Rumaz Naveed Qureshi <rumazqureshi@gmail.com>
2025-04-09 22:55:08 +0500

    initial commit

---
+++ bitwise.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+float a = 3,b = 2.2,c = 5,d = 3,g = 6,m = 8,q = 1,v = 7,x = 8,y = 3.5,r = 5.5;
+
+printf("Z=8.8*(a+b)*2/c-0.5+2*a/(q+r)/(a+b)*(1/m)=%f\n",8.8*(a+b)*2/c-0.5+2*a/(q+r)/(a+b)*(1/m));
+
+return 0;
+} 
