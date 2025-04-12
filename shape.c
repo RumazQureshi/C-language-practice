e6e26458a3be2f65f9c790b68c342da112032252
Rumaz Naveed Qureshi <rumazqureshi@gmail.com>
2025-04-12 11:03:17 +0500

    initial commit

---
+++ shape.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+ int main(){
+
+ int x= 1;
+ int y= 12;
+ int z= 123;
+ int a= 1234;
+ int b= 12345;
+ float f=23.67;
+ printf("\n %5d %5d %-5d",x,x,x,x);
+ printf("\n %5d %5d %-5d",y,y,y,y);
+ printf("\n %5d %5d %-5d",z,z,z,x);
+ printf("\n %5d %5d %-5d",a,a,a);
+ printf("\n %5d %5d %-5d",b,b,b);
+ printf("\n %20.4f",f);
+return 0;
+}
