a388084684ac9cef0cfe590bb93ec08cb3f0778b
Rumaz Naveed Qureshi <rumazqureshi@gmail.com>
2025-04-15 02:23:23 +0500

    initial commit

--- name.c
+++ name.c
@@ -10,5 +10,6 @@ printf("\n int %d \n float %f \n char %c \n double %lf\n",count,miles,letter,ato
 printf(" address in Integers :\n int %d \n float %d char %d \n double %d \n",&count,&miles,&letter,&atoms);
 printf(" address in Hex :\n int %x \n float %x \n char %x \n double %x \n",&count,&miles,&letter,&atoms);
 
+
 return 0;
 }
