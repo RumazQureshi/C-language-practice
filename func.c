5759f0d68bf8229ca5f9359eda1f50fb10533a60
Rumaz Naveed Qureshi <rumazqureshi@gmail.com>
2025-04-15 02:22:46 +0500

    initial commit

---
+++ func.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+float converttemp(float celcius);
+
+int main(){
+float far = converttemp(40);
+printf("farenhite is %f ", far);
+    return 0;
+ }
+float converttemp(float celcius){
+float far = celcius * 1.8 + 32;
+return far;
+}
