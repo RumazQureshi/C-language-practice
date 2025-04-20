e9a7d1ba9faf7edd43911ebb2fc67b3f7495a26f
Rumaz Naveed Qureshi <rumazqureshi@gmail.com>
2025-04-20 13:44:32 +0500

    Add files via upload

---
+++ d.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+float a = 3,b = 2.2,c = 5,d = 3,g = 6,m = 8,q = 1,v = 7,x = 8,y = 3.5,r = 5.5;
+
+printf("A=7.7*b*(x*y+a)/c-0.8+2*b/(x+a)*(1/y)=%f\n",7.7*b*(x*y+a)/c-0.8+2*b/(x+a)*(1/y));
+
+return 0;
+}   
